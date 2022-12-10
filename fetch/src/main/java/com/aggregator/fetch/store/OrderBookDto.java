d6ed44ed3e2e3ef3e76c4976f1f3271dce67ea7e
Rodion Altshuler <rodion.altshuler@gmail.com>
2022-12-10 01:59:09 +0100

    Fecth: how we migrate to new interface between api and fetch so it's better matches consumer's needs and easier to parse with low-level (non-Spring-Data) Redis clients:  1. fetch-0.0.2 running together with fetch-0.0.1-SNAPSHOT  2. ver 0.0.2 saved data in redis in another format  3. One class required changes to implement this change

--- fetch/src/main/java/com/aggregator/fetch/store/OrderBookDto.java
+++ fetch/src/main/java/com/aggregator/fetch/store/OrderBookDto.java
@@ -1,34 +1,44 @@
 package com.aggregator.fetch.store;
 
 import com.aggregator.domain.OrderBook;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 
-@RedisHash("OrderBook")
-public record OrderBookDto(@Id String exchangeId, List<MarketDto> markets) {
+@RedisHash("OrderBookString")
+public record OrderBookDto(@Id String exchangeId, String markets) {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     public static OrderBookDto fromOrderBook(OrderBook book) {
         var markets =
                 book.markets().values().stream().map(MarketDto::fromMarket).toList();
-
-        return new OrderBookDto(
-                book.exchangeId(),
-                markets);
+        try {
+            return new OrderBookDto(
+                    book.exchangeId(),
+                    mapper.writeValueAsString(markets));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public OrderBook toOrderBook() {
+        try {
+            var marketsList = mapper.readValue(this.markets, OrderBookDto.MarketDto[].class);
 
-        return new OrderBook(
-                exchangeId(),
-                markets.stream().collect(Collectors.toMap(
-                        MarketDto::market,
-                        MarketDto::toMarket))
-        );
-
+            return new OrderBook(
+                    exchangeId(),
+                    Arrays.stream(marketsList).collect(Collectors.toMap(
+                            MarketDto::market,
+                            MarketDto::toMarket)));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public record MarketDto(long timestamp, String market, double avgBid, double qtyBid, double avgAsk, double qtyAsk) {
